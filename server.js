225f315d4f2e2ba7bd0bae70f4feb3c1dc76b001
henrykamsi <kamsih924@gmail.com>
2026-08-14 00:20:26 +0100

    Fix Unicode escape sequence syntax error in server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const wss = new WebSocket.Server({ server });
 app.use(express.static(path.join(__dirname, 'public')));
 
 wss.on('connection', (ws) => {
-    // Spawn an interactive bash shell with custom color prompt
     const shell = pty.spawn('bash', [], {
         name: 'xterm-256color',
         cols: 80,
@@ -20,19 +19,16 @@ wss.on('connection', (ws) => {
         env: {
             ...process.env,
             TERM: 'xterm-256color',
-            // Custom prompt: Green path, Bright White text, Red errors, Yellow warnings
-            PS1: '\[\033[01;32m\]\u@hgt-cloud:\[\033[01;34m\]\w\[\033[00m\]\$ '
+            PS1: '\\[\\033[01;32m\\]\\u@hgt-cloud:\\[\\033[01;34m\\]\\w\\[\\033[00m\\]\\$ '
         }
     });
 
-    // Send PTY output directly to WebSocket client
     shell.onData((data) => {
         if (ws.readyState === WebSocket.OPEN) {
             ws.send(data);
         }
     });
 
-    // Receive keystrokes/commands from browser client
     ws.on('message', (msg) => {
         try {
             const data = JSON.parse(msg);
